eacf7cfeb17152c8e0c1f9f00d09acedd330b22b
amirmahdi <amirmahdi.ebrahimi8282@gmail.com>
2024-12-31 17:05:16 +0330

    video

--- pset5/twttr/test_twttr.py
+++ pset5/twttr/test_twttr.py
@@ -1,18 +1,22 @@
 from twttr import shorten
 
-def test_nonAlpha():
-    assert shorten('$twenty-five') == '$twnty-fv'
-    assert shorten('25 dollars') == '25 dllrs'
-    assert shorten('$25.00') == '$25.00'
+def test_shorten():
+    assert shorten("David") == "Dvd"
+    assert shorten("Harvard") == "Hrvrd"
 
-def test_consonants():
-    assert shorten('tstst') == 'tstst'
-    assert shorten('lgbtq') == 'lgbtq'
+def test_shorten_lower():
+    assert shorten("thisiscs50") == "thsscs50"
+    assert shorten("testtwitter") == "tsttwttr"
 
-def test_vowels():
-    assert shorten('aie') == ''
-    assert shorten('ou') == ''
 
-def test_capital():
-    assert  shorten('APPLE') == 'PPL'
+def test_shorten_upper():
+    assert shorten("HELLOWORD") == "HLLWRD"
+    assert shorten("THISISCS50") == "THSSCS50"
 
+def test_shorten_numeric():
+    assert shorten("Da1vid2") == "D1vd2"
+    assert shorten("Ha2R0V2ar76D") == "H2R0V2r76D"
+
+def test_shorten_punctuation():
+    assert shorten("Da!vid;") == "D!vd;"
+    assert shorten("Ha:R/V(ar)+D") == "H:R/V(r)+D"
--- pset5/twttr/twttr.py
+++ pset5/twttr/twttr.py
@@ -1,21 +1,12 @@
 def main():
-    inpt= input('Input: ')
-    devowelised_text = shorten(inpt)
-    print('Output:', devowelised_text)
+    str = input("Input: ")
+    print("Output:", shorten(str))
+def shorten(str):
+    new_str =""
+    for i in range(len(str)):
+        if(str[i]!="O" and str[i]!="o" and str[i]!="I" and str[i]!="i" and str[i]!="A" and str[i]!="a" and str[i]!="E" and str[i]!="e" and str[i]!="U" and str[i]!="u"):
+            new_str += str[i]
+    return new_str
 
-def shorten(word):
-    # vowels = ['a','e','i','o','u','A','E','I','O','U']
-    # the below line cause a bug
-    vowels = ['a','e','i','o','u',]
-    for vowel in vowels:
-        if vowel in word:
-            word = word.replace(vowel, '')
-    return word
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
